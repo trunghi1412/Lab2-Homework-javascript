33f2412ba98432cd55750d4884636abd27ee5510
trunghi1412 <91446424+trunghi1412@users.noreply.github.com>
2022-10-24 18:41:03 +0700

    Add files via upload

---
+++ lab2-ex1/hw1.js
@@ -0,0 +1,16 @@
+function goToProfile(){
+    const element = document.getElementById("Profile");
+    element.scrollIntoView();
+}
+function goToExperience(){
+    const element = document.getElementById("Experience");
+    element.scrollIntoView();
+}
+function goToAbilities(){
+    const element = document.getElementById("Abilities");
+    element.scrollIntoView();
+}
+function goToProject(){
+    const element = document.getElementById("skill_footer");
+    element.scrollIntoView();
+}
